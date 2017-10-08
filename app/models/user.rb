4df94a1df9a4f11dea0772de06522db3ffce1e26
Jason Dorn <apollo701@gmail.com>
2017-10-07 20:03:15 -0700

    Add todo model, connect to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_secure_token
+  has_many :todos, dependent: :destroy
 
   def invalidate_token
     update_columns(token: nil)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171007013739) do
+ActiveRecord::Schema.define(version: 20171008025012) do
+
+  create_table "todos", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "task"
+    t.boolean "completed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_todos_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
