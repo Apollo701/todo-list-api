294dd0b259866217385e3f848202966415cf17b0
Jason Dorn <apollo701@gmail.com>
2017-10-28 10:20:41 -0700

    Index user email and add validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :todos, dependent: :destroy
+
+  validates :email, uniqueness: { case_sensitive: false }
+  validates :password, length: { in: 6..20 }
+  before_save :downcase_email
+
+  private
+
+  def downcase_email
+    self.email.downcase!
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,14 @@
 require 'rails_helper'
 
 describe User, type: :model do
+  it "downcases emails before saving to the db" do
+    email = "BATMAN@bruce.com"
+    user_params = {
+      email: email,
+      password: "IAMTHEKNIGHT"
+    }
+    user = User.create(user_params)
+    expect(user.email)
+      .to eq email.downcase
+  end
 end
